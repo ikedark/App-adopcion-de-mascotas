b48147d2cc060f21998be21fb27575b04757590f
ikedark <david.v1795@gmail.com>
2023-03-11 16:56:04 -0700

    sidebar
    se elimino el intento de side bar y se optp por desarrollar el perfil mascota

---
+++ app/src/main/java/mx/edu/potros/adopaws/Perfil_Mascota.kt
@@ -0,0 +1,11 @@
+package mx.edu.potros.adopaws
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Perfil_Mascota : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil_mascota)
+    }
+}
