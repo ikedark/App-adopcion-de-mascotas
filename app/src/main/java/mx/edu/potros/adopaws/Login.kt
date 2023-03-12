cefe9059c18bc68e80e6e859b8b0615ec96da0c9
ikedark <david.v1795@gmail.com>
2023-03-11 21:26:48 -0700

    build
    test

--- app/src/main/java/mx/edu/potros/adopaws/Login.kt
+++ app/src/main/java/mx/edu/potros/adopaws/Login.kt
@@ -1,11 +1,20 @@
 package mx.edu.potros.adopaws
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btn_pregunta: Button = findViewById(R.id.btn_registrate)
+
+        btn_pregunta.setOnClickListener {
+            val intent: Intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/mx/edu/potros/adopaws/MainActivity.kt
+++ app/src/main/java/mx/edu/potros/adopaws/MainActivity.kt
@@ -1,11 +1,25 @@
 package mx.edu.potros.adopaws
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    var time: Int = 5
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        iniciar()
+    }
+
+    fun iniciar(){
+        while (time > 0){
+            time--
+            if (time==0){
+                val intent: Intent = Intent(this, Login::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
