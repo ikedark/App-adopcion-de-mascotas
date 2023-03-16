26f1166ffb7f61d505424037fccdee90bdbfdff3
ikedark <david.v1795@gmail.com>
2023-03-15 18:19:31 -0700

    cambios nombres logos

--- app/src/main/java/mx/edu/potros/adopaws/MainActivity.kt
+++ app/src/main/java/mx/edu/potros/adopaws/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    var time: Int = 50
+    var time: Int = 100000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
