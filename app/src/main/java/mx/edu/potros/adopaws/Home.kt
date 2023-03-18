9691b097f5a823555c7d1d09ee5ebf68aa1dfb68
Alberto <alberto.hernandez@potros.itson.edu.mx>
2023-03-17 18:48:29 -0700

    se editó el backend de home y se creó la clase mascota

--- app/src/main/java/mx/edu/potros/adopaws/Home.kt
+++ app/src/main/java/mx/edu/potros/adopaws/Home.kt
@@ -1,11 +1,70 @@
 package mx.edu.potros.adopaws
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
 
 class Home : AppCompatActivity() {
+
+    var lista: ArrayList<Mascota> = ArrayList<Mascota>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        agregarMascota()
+
+        var gridView: GridView = findViewById(R.id.gridView) as GridView
+
+        var adaptador: AdaptadorMascotas = AdaptadorMascotas(this,lista)
+        gridView.adapter = adaptador
+
     }
+
+    fun agregarMascota() {
+        lista.add(Mascota("Tiana", R.drawable.Tiana, "Hembra", 183))
+    }
+
+    private class AdaptadorMascotas: BaseAdapter {
+        var mascotas=ArrayList<Mascota>()
+        var contexto: Context?=null
+
+        constructor(contexto: Context, mascota:ArrayList<Mascota>){
+            this.mascotas=mascota
+            this.contexto=contexto
+        }
+
+        override fun getCount(): Int {
+            return mascotas.size
+        }
+
+        override fun getItem(p0: Int): Any {
+            return mascotas[p0]
+        }
+
+        override fun getItemId(p0: Int): Long {
+            return p0.toLong()
+        }
+
+        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+            var masc=mascotas[p0]
+            var inflador= LayoutInflater.from(contexto)
+            var vista=inflador.inflate(R.layout.mascota_view,null)
+
+            var imagen=vista.findViewById(R.id.mascota_img) as ImageView
+            var nombre=vista.findViewById(R.id.nombre_mascota) as TextView
+            var sexo = vista.findViewById(R.id.sexo_mascota) as TextView
+            var likes=vista.findViewById(R.id.likes_mascota) as TextView
+
+            imagen.setImageResource(masc.image)
+            nombre.setText(masc.nombre)
+            sexo.setText(masc.sexo)
+            likes.setText(masc.likes)
+            return vista
+        }
+    }
+
 }
---
+++ app/src/main/java/mx/edu/potros/adopaws/Mascota.kt
@@ -0,0 +1,6 @@
+package mx.edu.potros.adopaws
+
+data class Mascota(var nombre:String,
+                   var image: Int,
+                   var sexo: String,
+                   var likes:Int)
