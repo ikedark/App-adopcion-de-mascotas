86de19aa340ad46e7d008ee0b5a6448d67a82dc2
David Picos <david.v1795@gmail.com>
2023-03-25 00:42:05 -0700

    Botones google y facebook

--- app/src/main/java/mx/edu/potros/adopaws/Login.kt
+++ app/src/main/java/mx/edu/potros/adopaws/Login.kt
@@ -1,14 +1,57 @@
 package mx.edu.potros.adopaws
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+
 
 class Login : AppCompatActivity() {
+
+    lateinit var callbackManager: CallbackManager
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    val RC_SIGN_IN = 343
+    val LOG_OUT = 234
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        val sign_in_button : LoginButton = findViewById(R.id.sign_in_button)
+
+        sign_in_button.setOnClickListener {
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
+
+
+        // Facebook Login
+        callbackManager = CallbackManager.Factory.create();
 
         val btn_pregunta: Button = findViewById(R.id.btn_registrate)
         val btn_iniciarSesion: Button = findViewById(R.id.btn_login)
@@ -23,7 +66,71 @@ class Login : AppCompatActivity() {
             startActivity(intent)
         }
 
+        val loginButton : LoginButton = findViewById(R.id.login_button)
+        loginButton.setReadPermissions(listOf("public_profile", "email"))
+        // If you are using in a fragment, call loginButton.setFragment(this)
 
+        // Callback registration
+        loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult) {
+                Log.d("TAG", "Success Login")
+                // Get User's Info
+            }
 
+            override fun onCancel() {
+                Toast.makeText(this@Login, "Login Cancelled", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onError(exception: FacebookException) {
+                Toast.makeText(this@Login, exception.message, Toast.LENGTH_LONG).show()
+            }
+        })
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        updateUI(account)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if ( requestCode == RC_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+
+        if (requestCode == LOG_OUT){
+            signOut()
+        }
+    }
+
+    private fun signOut() {
+        mGoogleSignInClient.signOut()
+            .addOnCompleteListener(this, OnCompleteListener<Void?>{
+                Toast.makeText(this,"Sesión terminada", Toast.LENGTH_SHORT).show()
+            })
+    }
+
+    private fun handleSignInResult(completeTask: Task<GoogleSignInAccount>){
+        try {
+            val account = completeTask.getResult(ApiException::class.java)
+            updateUI(account)
+        }catch (e: ApiException){
+            Log.w("test_signin", "signInResult: failed code=" + e.statusCode)
+        }
+    }
+
+    private fun updateUI(acct: GoogleSignInAccount?){
+        if (acct != null){
+            val intent = Intent(this, Home::class.java)
+            intent.putExtra("id",acct.getId())
+            intent.putExtra("name",acct.getDisplayName())
+            intent.putExtra("email",acct.getEmail())
+            startActivityForResult(intent,LOG_OUT)
+        }
     }
 }
---
+++ app/src/main/java/mx/edu/potros/adopaws/configuracionGeneral.kt
@@ -0,0 +1,11 @@
+package mx.edu.potros.adopaws
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class configuracionGeneral : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_configuracion_general)
+    }
+}
