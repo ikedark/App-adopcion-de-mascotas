1aa03b633aaac09604c5c98bd510b1223cc33de1
David Picos <david.v1795@gmail.com>
2023-03-27 11:45:08 -0700

    cambios

--- app/src/main/java/mx/edu/potros/adopaws/perfilUsuario.kt
+++ app/src/main/java/mx/edu/potros/adopaws/perfilUsuario.kt
@@ -7,5 +7,15 @@ class perfilUsuario : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil_usuario)
+
+        var tv_nombre: TextView = findViewById(R.id.tv_nombre_usuario)
+
+        val bundle = intent.extras
+
+        if (bundle != null){
+            val name = bundle.getString("name")
+            tv_nombre.setText(name)
+        }
+
     }
 }
