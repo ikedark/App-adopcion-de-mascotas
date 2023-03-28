37260972bee79d90dc4cd2ccd3e79d42989aa0ef
David Picos <david.v1795@gmail.com>
2023-03-27 18:03:51 -0700

    registro

--- app/src/main/java/mx/edu/potros/adopaws/Register.kt
+++ app/src/main/java/mx/edu/potros/adopaws/Register.kt
@@ -1,19 +1,53 @@
 package mx.edu.potros.adopaws
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
+import android.app.Dialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.icu.util.Calendar
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Spinner
+import android.widget.DatePicker
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
+import com.google.android.material.internal.ContextUtils.getActivity
+
+class DatePickerFragment : DialogFragment() {
+
+    private var listener: DatePickerDialog.OnDateSetListener? = null
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        return DatePickerDialog(activity, listener, year, month, day)
+    }
+
+    companion object {
+        fun newInstance(listener: DatePickerDialog.OnDateSetListener): DatePickerFragment {
+            val fragment = DatePickerFragment()
+            fragment.listener = listener
+            return fragment
+        }
+    }
+
+}
 
 class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        val sp_dias: Spinner = findViewById(R.id.sp_dia)
+        /*val sp_dias: Spinner = findViewById(R.id.sp_dia)
         val sp_mes: Spinner = findViewById(R.id.sp_mes)
-        val sp_anio: Spinner = findViewById(R.id.sp_año)
+        val sp_anio: Spinner = findViewById(R.id.sp_año)*/
+
+        val fecha : EditText = findViewById(R.id.fecha_nacimiento)
+
+        fecha.setOnClickListener { showDatePickerDialog() }
 
         val btnpregunta : Button = findViewById(R.id.btn_iniciaSesión)
 
@@ -21,8 +55,16 @@ class Register : AppCompatActivity() {
             val intent: Intent = Intent(this, Login::class.java)
             startActivity(intent)
         }
+    }
 
 
+    private fun showDatePickerDialog() {
+        val newFragment = DatePickerFragment.newInstance(DatePickerDialog.OnDateSetListener { _, year, month, day ->
+            // +1 because January is zero
+            val selectedDate = day.toString() + " / " + (month + 1) + " / " + year
+            etBirthDate.setText(selectedDate)
+        })
 
+        newFragment.show(supportFragmentManager, "datePicker")
     }
 }
