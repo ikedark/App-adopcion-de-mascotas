1092d4e7c0f34671555c36efd3da5648c5944f31
Elmamis69 <60937298+Elmamis69@users.noreply.github.com>
2023-03-30 20:20:15 -0700

    Mapa prueba

---
+++ app/src/main/java/mx/edu/potros/adopaws/MapaPrueba.kt
@@ -0,0 +1,41 @@
+package mx.edu.potros.adopaws
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapaPrueba : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var map: GoogleMap
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mapaprueba)
+        createFragment()
+    }
+
+    private fun createFragment() {
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        createMarker()
+    }
+
+    private fun createMarker() {
+        val coordinates = LatLng(27.496218, -109.971497)
+        val marker = MarkerOptions().position(coordinates).title("Mi ubicación")
+        map.addMarker(marker)
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(coordinates, 15f),
+            4000,
+            null
+        )
+    }
+}
